57800dddc2754a6c34c1e9ea116ec8eab494704a
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2023-12-13 16:24:56 +0700

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -18,7 +18,7 @@ with Data:
    st.write("Fitur Fitur yang ada diantaranya:")
    st.text("1) NIM\n2) Judul\n3) Abstrak\n4) Program Studi\n5) Penulis\n6) Dosen Pembimbing 1\n7) Dosen Pembimbing 2\n8) Label")
    st.subheader("Data")
-   data = pd.read_csv("DF_PTA.csv")
+   data = pd.read_csv("berita.csv")
    st.write(data)
 
 with lda:
@@ -40,7 +40,7 @@ with lda:
       submit() 
 
 with Model:
-    tf = pd.read_csv("df_tf.csv")
+    tf = pd.read_csv("berita_tf.csv")
     st.subheader("Jumlah Topik yang Anda Gunakan : " + str(topik))
     st.write("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 5")
     lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
@@ -56,10 +56,10 @@ with Model:
     y = df['Label'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model1 = KNeighborsClassifier(5)
-    # Pelatihan model KNN dengan data pelatihan
+    # Pelatihan model Random Forest dengan data pelatihan
+    model1 = RandomForestClassifier(n_estimators=100, random_state=42)
     model1.fit(X_train, y_train)
-
+   
     model2 = MultinomialNB()
     # Pelatihan model Naive Bayes dengan data pelatihan
     model2.fit(X_train, y_train)
@@ -69,14 +69,14 @@ with Model:
     model3.fit(X_train, y_train)
 
     st.write("Pilih metode yang ingin anda gunakan :")
-    met1 = st.checkbox("KNN")
+    met1 = st.checkbox("Random Forest")
     met2 = st.checkbox("Naive Bayes")
     met3 = st.checkbox("Decision Tree")
     submit2 = st.button("Pilih")
 
     if submit2:      
         if met1:
-            st.write("Metode yang Anda gunakan Adalah KNN")
+            st.write("Metode yang Anda gunakan Adalah Random Forest")
             # Prediksi label kelas pada data pengujian
             y_pred = model1.predict(X_test)
             # Mengukur akurasi model
@@ -100,7 +100,7 @@ with Model:
             st.write("Anda Belum Memilih Metode")
 
 with Implementasi:
-    data = pd.read_csv("DF_PTA.csv")
+    data = pd.read_csv("berita.csv")
     data['Abstrak'].fillna("", inplace=True)
     count_vectorizer = CountVectorizer(max_df=0.95, min_df=2)
     
@@ -128,15 +128,15 @@ with Implementasi:
         return cleaned_text
 
     st.subheader("Implementasi")
-    st.write("Masukkan Abstrak yang Ingin Dianalisis:")
-    user_abstract = st.text_area("Abstrak", "")
+    st.write("Masukkan Content yang Ingin Dianalisis:")
+    user_abstract = st.text_area("Content", "")
 
-    if user_abstract:
-        # Preproses abstrak
-        preprocessed_abstract = preprocess_text(user_abstract)
+    if user_content:
+        # Preproses content
+        preprocessed_content = preprocess_text(user_abstract)
 
         # Fit vocabulary dengan data latih
-        count_vectorizer.fit(data['Abstrak'])
+        count_vectorizer.fit(data['Content'])
 
         # Transform abstrak pengguna dengan count_vectorizer
         user_tf = count_vectorizer.transform([preprocessed_abstract])
